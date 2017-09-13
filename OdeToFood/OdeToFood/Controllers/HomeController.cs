428310a8b6af7a4dbc289af12032bd5a9e97fd13
Redae Yihdego <redae2014@yahoo.com>
2017-09-13 14:08:22 -0400

    NES-2300 : Created OdeToFood Project

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+using OdeToFood.ViewModels;
+using System.Linq;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
+
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
+        {
+            _restaurantData = restaurantData;
+            _greeter = greeter;
+        }
+        public IActionResult Index()
+        {
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.Getall();
+            model.CurrentMessage = _greeter.GetGreeting();
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var model = _restaurantData.Get(id);
+            if(model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+    }
+}
